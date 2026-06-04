95a75078aa1894ec1e2627849b8879b2411059e6
Anna Puteine <anna.puteine@chi.lv>
2026-06-04 12:51:41 +0300

    Favourites added

---
+++ lib/model/favourites/favourite_object.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favourite_object.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FavouriteObject _$FavouriteObjectFromJson(Map<String, dynamic> json) =>
+    FavouriteObject(id: json['id'] as String);
+
+Map<String, dynamic> _$FavouriteObjectToJson(FavouriteObject instance) =>
+    <String, dynamic>{'id': instance.id};
